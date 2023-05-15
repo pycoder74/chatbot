f8901784293197467d3ab2aac68f23f36011654a
pycoder74 <105924451+pycoder74@users.noreply.github.com>
2023-05-15 18:14:10 +0100

    Update main.py
    Moved arrays into other files

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ try:
     from getweather import get_conditions, get_current_temp
     import tracemalloc
     import asyncio
-except ModuleNotFoundError:
+    from weather_qs_arrays import*
+    from greeting_arrays import*
+    import weather_qs_arrays as wqa
+    import greeting_arrays as ga
+except ModuleNotFoundError as err:
+    import traceback
+    print(traceback.format_exc())
     import installer as install
     install.install_package('numpy asyncio python_weather')
     print('requirements installed')
@@ -14,19 +20,18 @@ except ModuleNotFoundError:
     from getweather import get_conditions, get_current_temp
     import tracemalloc
     import asyncio
-weather_cmds = np.array(["What is the weather like?", "What is the current weather?", "What is the weather right now?"])
-temp_qs = np.array(["What is the temperature today?", "Is it hot or cold right now?", "What is the temperature like?", "What is the temperature right now?"])
-greetings = np.array(["Hello.", "Hi!", "Hey!", "Hey there!"])
-questions = np.array(["How can I help?", "What can I do for you?", "What do you need help with?"])
-Hellos = np.concatenate((greetings, questions))
-
-greeting = np.random.choice(greetings)
-question = np.random.choice(questions)
+    from weather_qs_arrays import*
+    from greeting_arrays import*
+    import weather_qs_arrays as wqa
+    import greeting_arrays as ga
+    
+greeting = np.random.choice(ga.greetings)
+question = np.random.choice(ga.questions)
 
 task = input(f"{greeting} {question}\n>> ")
 print(f"Task entered: {task}")
 
-if task in weather_cmds:
+if task in wqa.weather_cmds:
     location = input("Enter country:")
     tracemalloc.start()
     print("Calling get_conditions()...")
@@ -34,7 +39,7 @@ if task in weather_cmds:
     print(forecast)
     tracemalloc.stop()
 
-if task in temp_qs:
+if task in wqa.temp_qs:
     location = input("Enter country: ")
     tracemalloc.start()
     print("Calling get_current_temp()...")

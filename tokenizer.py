b57417368a9c8998fa85e56f83e17738b9d66854
pycoder74 <105924451+pycoder74@users.noreply.github.com>
2023-05-17 17:04:17 +0100

    Update tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -1,8 +1,8 @@
 def tokenize(string):
-    string.split()
+    return string.split()
 
 if __name__ == '__main__':
-    hi=('Hello there')
-    hi=tokenize(hi)
+    hi = 'Hello there'
+    hi = tokenize(hi)
     print(hi)
 

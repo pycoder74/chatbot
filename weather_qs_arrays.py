56c9b6bc62e27dec692a4288fa08ed39aff7371e
pycoder74 <105924451+pycoder74@users.noreply.github.com>
2023-05-20 15:50:15 +0100

    Update weather_qs_arrays.py

--- weather_qs_arrays.py
+++ weather_qs_arrays.py
@@ -1,3 +1,3 @@
 import numpy as np
 weather_words = np.array(['Weather', 'weather', 'Conditions']).astype(str)
-temp_words = np.array(["Temperature"]).astype(str)
+temp_words = np.array(["Temperature", "Temp", "temp", "temperature"]).astype(str)

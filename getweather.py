0c3645cefc87650b589578a0c6283af5e29dce60
pycoder74 <105924451+pycoder74@users.noreply.github.com>
2023-05-26 08:58:07 +0100

    Update getweather.py

--- getweather.py
+++ getweather.py
@@ -1,75 +1,30 @@
-import python_weather
-import tracemalloc
-import random
 import numpy as np
-import asyncio
-import aiohttp
+import datetime as dt
+from datetime import datetime
+days = (["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"])
+time_words = np.array(["Tomorrow"])
+time_words=np.append(days, time_words)
+print(f"Time words: {time_words}")
+current_date=datetime.now()
+print(current_date.strftime('%A'))
+
+
+import urllib.request
 import sys
 
-async def get_conditions(location):
-    url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}"
-    params = {
-        "unitGroup": "metric",
-        "include": "current",
-        "key": "5H2F2M6K2X4E9LXKH7UP3DNMQ",
-        "contentType": "json"
-    }
-
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, params=params) as response:
-            if response.status != 200:
-                print('Unexpected Status code:', response.status)
-                return None
-
-            jsonData = await response.json()
-
-            # Extracting the current temperature
-            current_temp = jsonData['currentConditions']['temp']
-            current_temp = f"{current_temp}°C"
-
-            # Extracting the weather conditions
-            weather_conditions = jsonData['currentConditions']['conditions']
-
-            weather_outputs = np.array([
-                f"{location} is {current_temp} and {weather_conditions}",
-                f"In {location}, it is {current_temp} and {weather_conditions}.",
-                f"{location} is currently {weather_conditions} with a temperature of {current_temp}."
-            ])
-
-            return np.random.choice(weather_outputs)
-
-
-async def get_current_temp(location):
-    url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}"
-    params = {
-        "unitGroup": "metric",
-        "include": "current",
-        "key": "5H2F2M6K2X4E9LXKH7UP3DNMQ",
-        "contentType": "json"
-    }
-
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, params=params) as response:
-            if response.status != 200:
-                print('Unexpected Status code:', response.status)
-
-            jsonData = await response.json()
-
-            # Extracting the current temperature
-            current_temp = jsonData['currentConditions']['temp']
-            current_temp = f"{current_temp}°C"
-            temp_outputs=np.array([f"In {location}, it is {current_temp}.", f"{location} is currently {current_temp}."])
-            # Print the current temperature
-            return np.random.choice(temp_outputs)
-
-
-async def main():
-    location = 'Bristol'
-
-    result = await get_conditions(location)
-    print(result)
-
-    await get_current_temp(location)
-
-if __name__ == '__main__':
-    asyncio.run(main())
+import json
+                
+try: 
+  ResultBytes = urllib.request.urlopen("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Bristol?unitGroup=metric&include=day&key=5H2F2M6K2X4E9LXKH7UP3DNMQ&contentType=json")
+  
+  # Parse the results as JSON
+  jsonData = json.load(ResultBytes)
+  print(jsonData)
+except urllib.error.HTTPError  as e:
+  ErrorInfo= e.read().decode() 
+  print('Error code: ', e.code, ErrorInfo)
+  sys.exit()
+except  urllib.error.URLError as e:
+  ErrorInfo= e.read().decode() 
+  print('Error code: ', e.code,ErrorInfo)
+  sys.exit()
